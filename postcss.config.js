a207774349767b4ee9d78780189738357b5e0839
sulayibraheem <sulayibraheem@gmail.com>
2021-07-08 21:22:16 +0100

    modified postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,11 @@
 const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: [".src/**/*.{js,vue,html}"],
+  content: [
+    ".src/components/**/*.{js,vue,html}",
+    ".src/store/**/*.{js,vue,html}",
+    ".src/router/**/*.{js,vue,html}",
+    ".src/assets/Images/**/*.{js,vue,html}",
+    ".src/views/**/*.{js,vue,html}",
+  ],
   defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
 });
 
