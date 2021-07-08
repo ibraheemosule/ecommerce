a110d06701282e8893c4002480e54af0e9755f8e
sulayibraheem <sulayibraheem@gmail.com>
2021-07-08 21:05:49 +0100

    changed font family

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,7 @@ export default {
 };
 </script>
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;800&display=swap");
 body {
   font-family: "Lato", sans-serif !important;
   letter-spacing: 0.1em;
